3a88a2cb6c98bb7f8c0d9791724287207a7a8eb2
Mohan <mdvankar1990@gmail.com>
2022-11-24 09:45:58 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,166 @@
+import os
+import time
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, request, Response
+from flask_sqlalchemy import SQLAlchemy
+from telebot import TeleBot
+from pubsub import youtube_search_by_channel, subscribe
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE']
+db = SQLAlchemy(app)
+Token = os.environ["TELE_Token"]
+bot = TeleBot(Token, parse_mode=None)
+bot.remove_webhook()
+bot.set_webhook(url=os.environ['BASE_URL'])
+
+user_sub = db.Table("user_sub", db.Column('user_id', db.Integer, db.ForeignKey("user_table.user_id")),
+                    db.Column('channel_id', db.Integer, db.ForeignKey("subscription_table.channel_id")))
+
+
+class Users(db.Model):
+    __tablename__ = "user_table"
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_chat_id = db.Column(db.Integer, nullable=False, unique=True)
+    user_name = db.Column(db.String(150), nullable=False)
+
+
+class Subscription(db.Model):
+    __tablename__ = "subscription_table"
+    channel_id = db.Column(db.String(1000), primary_key=True)
+    channel_name = db.Column(db.String(50), nullable=False)
+    users = db.relationship("Users", secondary="user_sub", backref="subscriptions")
+
+
+def tel_parse_message(message):
+    # print("message-->", message)
+
+    try:
+        chat_id = message['message']['chat']['id']
+        txt = message['message']['text']
+    except KeyError:
+        chat_id = message['edited_message']['chat']['id']
+        txt = message['edited_message']['text']
+
+    if "/" in txt:
+        command = txt.split(" ")[0].strip("/")
+        args = [item for item in txt.split(" ")]
+    else:
+        command = None
+        args = None
+    # print("chat_id-->", chat_id)
+    # print("txt-->", txt)
+    # print("command-->", command)
+    # print("args-->", args)
+    return chat_id, txt, command, args
+
+
+
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{Token}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+
+    r = requests.post(url, json=payload)
+
+    return r
+
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    db.create_all()
+    if request.method == 'POST':
+        msg = request.get_json()
+        print(msg)
+        chat_id, txt, command, args = tel_parse_message(msg)
+        current_user = Users.query.filter_by(user_chat_id=chat_id).first()
+        if not current_user:
+            new_user = Users()
+            new_user.user_name = request.json['message']['chat']['first_name']
+            new_user.user_chat_id = chat_id
+            db.session.add(new_user)
+            db.session.commit()
+            current_user = new_user
+        if command is None:
+            if txt == "hi":
+                tel_send_message(chat_id, "Hello, world!")
+            else:
+                tel_send_message(chat_id, 'Nothing much')
+        else:
+            if command.title() == "Subscribe" or command.title() == "S":
+                print(args[1].title())
+                if args[1].title() == "Name":
+                    query = " ".join(args[2:])
+                    channel_in_db = Subscription.query.filter_by(channel_name=query).first()
+                    flag = True
+                elif args[1].title() == "Id":
+                    query = args[2]
+                    channel_in_db = Subscription.query.get(query)
+                    flag = False
+                else:
+                    tel_send_message(chat_id,"command is incorrect")
+                    return "Error", 200
+                if channel_in_db:
+                    if current_user in channel_in_db.users:
+                        tel_send_message(chat_id,"You are already subscriber.")
+                    else:
+                        current_user.subscriptions.append(channel_in_db)
+                        db.session.add(current_user)
+                        db.session.commit()
+                        tel_send_message(chat_id,"You are added as subscriber.")
+                else:
+                    if flag:
+                        sel_channel_id = youtube_search_by_channel(query)
+
+                    else:
+                        sel_channel_id = query
+
+
+                    if not (sel_channel_id == " " or sel_channel_id == "Error occurred. Search again"):
+                        if subscribe(sel_channel_id, 1) == 202:
+                            new_sub = Subscription()
+                            new_sub.channel_name = query
+                            new_sub.channel_id = sel_channel_id
+                            new_sub.users.append(current_user)
+                            db.session.add(new_sub)
+                            db.session.commit()
+                            tel_send_message(chat_id, "subscribed")
+                    else:
+                        print(sel_channel_id)
+                        tel_send_message(chat_id, "Channel not found or error occurred while searching")
+        return Response('ok', status=200)
+    else:
+        return "Welcome"
+
+
+@app.route("/feed/<feed_id>", methods=['GET', 'POST'])
+def feed(feed_id):
+    if request.method == 'GET':
+        if request.args != {}:
+            return request.args['hub.challenge']
+        return "<h1>Hello<h1>"
+    if request.method == "POST":
+        data = request.data.decode()
+        soup = BeautifulSoup(data, 'lxml')
+        soup = BeautifulSoup(data, "lxml")
+        channel = str(soup.find("uri")).strip("<uri>").strip("</")
+        video_id = str(soup.find("id")).strip("<id>").strip("</").split(":")[2]
+        video_title = str(soup.find_all("title")[1]).strip("<title>").strip("</")
+        name = str(soup.find("name")).strip("<name>").strip("</")
+        channel_id = channel.split("/")[4]
+        broadcast_channel = Subscription.query.get(channel_id)
+        if broadcast_channel.channel_id == broadcast_channel.channel_name:
+            broadcast_channel.channel_name = name
+        print(f"{channel}{video_id}{video_title}{name}{channel_id}")
+        for item in broadcast_channel.users:
+            msg = f"Hey {item.user_name},\n{name} have {video_title} check that out from link below:\n{channel}"
+            tel_send_message(item.user_chat_id, msg)
+        return "Success", 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pubsub.py
@@ -0,0 +1,53 @@
+import os
+
+import requests
+
+youtube_search_url = "https://www.googleapis.com/youtube/v3/search"
+google_api_token = os.environ['GOOGLE_API_KEY']
+base_yt_url = "https://www.youtube.com/xml/feeds/videos.xml?channel_id="
+base_callback_url = os.environ['BASE_URL'] + "/feed/"
+subscribe_url = "https://pubsubhubbub.appspot.com/subscribe"
+
+
+# To subscribe to pubsubhubbub using channel id and feed id
+def subscribe(channel_id: str, feed_id):
+    yt_url = base_yt_url + channel_id
+    callback_url = base_callback_url + str(feed_id)
+    header = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+        "content-type": "application/x-www-form-urlencoded",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "sec-fetch-dest": "document"}
+    body = {"hub.callback": callback_url,
+            "hub.topic": yt_url,
+            "hub.verify": "async",
+            "hub.mode": "subscribe",
+            "hub.verify_token": "",
+            "hub.secret": "",
+            "hub.lease_seconds": ""}
+    response = requests.post(subscribe_url, headers=header, data=body)
+    response.raise_for_status()
+    return response.status_code
+
+
+def youtube_search_by_channel(my_query: str):
+    query = {"part": "snippet",
+             "q": my_query,
+             "type": "Channel",
+             "key": google_api_token,
+             "maxResults": 50}
+
+    response = requests.get(url=youtube_search_url, params=query)
+    try:
+        response.raise_for_status()
+    except:
+        return "Error occured. Search again"
+    items = response.json()['items']
+    sel_channel_id = " "
+    for item in items:
+        if item['snippet']['title'] == my_query:
+            sel_channel_id = item['snippet']['channelId']
+
+    return sel_channel_id
